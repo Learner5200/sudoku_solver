a8a23bf5757870fe82730116b14778ee9e8b4e7d
Chris Cooksley <christopher.cooksley@gmail.com>
2018-09-21 15:42:38 +0100

    refactoring

--- solver.rb
+++ solver.rb
@@ -33,26 +33,34 @@ class Grid
   def get_box(name)
     char, num = name[0], name[1].to_i
     index = (num-1)/3 # turns the numerical identifier for an item's row (1-9) into an identifier for which column of boxes it belongs to (1-3)
-
     ["ABC", "DEF", "GHI"].each {|string| @box_name = string.downcase[index] if string.include?(char)} # gets box name (named as above) from the above
     return @sections["boxes"][@box_name]
   end
 
-  def reduce_possibilities
+
+  def reduce_possibilities(item_name)
+    # creates array of possibilities and eliminates any that are present in the item's row, column or box
+    item_row = sections["rows"][item_name[0]]
+    item_col = sections["columns"][item_name[1]]
+    item_box = get_box(item_name)
+    @possibilities[item_name] = (1..9).to_a
+    (item_row | item_col | item_box).each { |number| @possibilities[item_name].delete(number) if number != 0 }
+  end
+
+  def update(item_name, row_index, column_index)
+    # updates empty square in grid if there is only one logical possibility
+    if @possibilities[item_name].length == 1
+      @grid[row_index][column_index] = @possibilities[item_name][0]
+    end
+  end
+
+  def fill
     @grid.each_with_index do |row, row_index|
       row.each_with_index do |item, column_index|
         if item == 0
           item_name = "ABCDEFGHI"[row_index] + "123456789"[column_index]
-          item_row = sections["rows"][item_name[0]]
-          item_col = sections["columns"][item_name[1]]
-          item_box = get_box(item_name)
-          @possibilities[item_name] = (1..9).to_a
-          (item_row | item_col | item_box).each { |number| @possibilities[item_name].delete(number) if number != 0 }
-
-          if @possibilities[item_name].length == 1
-            @grid[row_index][column_index] = @possibilities[item_name][0]
-          end
-
+          reduce_possibilities(item_name)
+          update(item_name, row_index, column_index)
         end
       end
     end
@@ -68,7 +76,7 @@ class Grid
         puts "Unable to solve after 100 iterations: returning current grid"
         return @grid
       end
-      reduce_possibilities
+      fill
     end
 
     return @grid
